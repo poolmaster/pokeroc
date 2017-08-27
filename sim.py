d7c86d6ce45839b9fcf55d69efd5e905fee0f7b3
poolmaster <yong.void.li@gmail.com>
2017-08-27 18:18:12 -0500

    support for odds calculation

--- sim.py
+++ sim.py
@@ -2,40 +2,42 @@
 
 from __future__ import division
 
+import random
+from datetime import datetime
 from param import *
 from dealer import Dealer
+from bookeeper import Bookeeper
 
-
-simAllHands = 0
+dbName = "__stats.db"
+numAllHand = 0
 numPlayer = 2
-cntTotal = 0 #total hands
-cntRes = [] #win/tie/loose count for hands
-odds = []
+numPuppet = 0
+puppetHands = []
+propCommunity = []
+cntTotalHands = 0 #total hands
 
+#todo
+#add command line support
 
 #initialize
-dealer = Dealer(numPlayer, 0)
-for i in xrange(NUM_HAND):
-    cntRes.append([0,0])
-    odds.append(0)
+random.seed(datetime.now())
+dealer = Dealer(numPlayer, numPuppet)
+for hand in puppetHands:
+    dealer.addPropCards(hand)
+dealer.addPropCards(propCommunity)
+bookeeper = Bookeeper(dbName)
 
 #simulate
-for i in xrange(cntTotal):
+for i in xrange(numAllHand):
     dealer.play()
     dealer.judge()
-    for player in dealer.players:
-        if not player.isPuppet and not simAllHands:
-            continue
-        res = dealer.getResult(player) 
-        if res != LOST:
-            handIdx = getHandIdx(player.cards) 
-            cntRes[handIdx][res] += 1
+    bookeeper.update()
 
-for i in xrange(NUM_HAND):
-    if cntRes[i] == [0, 0]:
-        continue
-    odds[i][TIE] = cntRes[TIE]/cntTotal
-    odds[i][WIN] = cntRes[WIN]/cntTotal
+#get odds
+for hand in puppetHands:
+    winOdd = bookeeper.getWinOdd(hand)
+    tieOdd = bookeeper.getTieOdd(hand)
+    print hand + " win: " + winOdd + " tie: " + tieOdd
 
 #for i in xrange(len(dealer.players)):
 #    dealer.displayPlayer(i)
