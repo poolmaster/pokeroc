74d4d27c3a0f52cc6564a026b23fdcc0c870ad29
poolmaster <yong.void.li@gmail.com>
2017-09-12 16:28:58 -0500

    add record functions

--- bookeeper.py
+++ bookeeper.py
@@ -4,33 +4,47 @@
 # interact with DB
 
 from __future__ import division
-from utils import *
 from collections import defaultdict
+from dealer import Dealer
+import utils
 
 class Bookeeper:
+    #instance variable:
+    #cntPlayerWins, cntCard: for testing 
+    #below are all dictionary of hand-str
+    #cntHandWins, cntHandTies, dbWinOdss, dbTieOdds, dbCnts, dbName 
     def __init__(self, dbName):
-        self.cntAll = 0
-        #self.cntHandWins = [0 for x in xrange(NUM_HAND)] 
-        #self.cntHandTies = [0 for x in xrange(NUM_HAND)]
-        self.cntHandWins = {}
+        self.cntPlayerWins = defaultdict(int) #allow direct access without creation
+        self.cntCard = defaultdict(int)
+        self.cntHandWins = defaultdict(int) 
         self.cntHandTies = defaultdict(int)
-        self.cntHandTies = {}
-        self.cntHandWins = defaultdict(int)
+        self.dbName = dbName
         self.dbWinOdds = {}
         self.dbTieOdds = {}
         self.dbCnts = {}
-        self.dbName = dbName
+ 
+    def recordPlayer(self, dealer):
+        winners = dealer.winner
+        for player in winners:
+            self.cntPlayerWins[player.id] += 1
 
-    def update(self, players, winners): 
-        cntAll += 1
-        tie = False 
-        for player in winner:
-            hand = getHandStr(player.cards)
+    def recordCard(self, dealer):
+        for player in dealer.players:
+            for card in player.cards:
+                self.cntCard[card] += 1
+        for card in dealer.community:
+            self.cntCard[card] += 1
+
+    def recordHand(self, dealer): 
+        winners = dealer.winner
+        tie = (len(winners) > 1)
+        for player in winners:
+            hand = utils.getHandStr(player.cards)
             if not tie:
-                cntHandWins[hand] += 1
+                self.cntHandWins[hand] += 1
             else:
-                cntHandTies[hand] += 1
-
+                self.cntHandTies[hand] += 1
+    
     def writeDB(self):
         dbFile = open(dbName, "w")
         #todo
@@ -43,20 +57,20 @@ class Bookeeper:
             if entry == "" or entry[0] == '#':
                 continue
             elements = [x.strip() for x in entry.split(',')]
-            dbWinOdds[elements[0]] = elements[1]
-            dbTieOdds[elements[0]] = elements[2]
-            dbCnts[elements[0]] = elements[3]
+            self.dbWinOdds[elements[0]] = elements[1]
+            self.dbTieOdds[elements[0]] = elements[2]
+            self.dbCnts[elements[0]] = elements[3]
         dbFile.close()
         
-    def getWinOdd(self, hand): 
-        if dbCnt[hand] > cntHandWins[hand]:
-            return dbWinOdds[hand]
+    def getWinOdd(self, hand, cntAll=1): 
+        if self.dbCnts[hand] > self.cntHandWins[hand]:
+            return self.dbWinOdds[hand]
         else:
-            return cntHandWins[hand] / cntAll 
+            return self.cntHandWins[hand] / cntAll 
     
-    def getTieOdd(self, hand): 
-        return cntHandTies[hand] / cntAll 
+    def getTieOdd(self, hand, cntAll=1): 
+        return self.cntHandTies[hand] / cntAll 
 
     def getCnt(self, hand):
-        return dbCnt[hand]
+        return self.dbCnts[hand]
 
